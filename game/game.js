81dfa4d9117dbecd144adf34af4fc3b9a83cccff
Kamran Ayub <kamran.ayub@gmail.com>
2015-08-21 23:25:34 -0500

    Compile

--- game/game.js
+++ game/game.js
@@ -5,7 +5,33 @@ var Resources = {
     TextureTreasure: new ex.Texture("images/treasure.png"),
     TextureMap: new ex.Texture("images/map.png")
 };
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    __.prototype = b.prototype;
+    d.prototype = new __();
+};
+var Hero = (function (_super) {
+    __extends(Hero, _super);
+    function Hero(x, y, width, height, color) {
+        _super.call(this, x, y, width, height, color);
+        // this.addDrawing(Resources.TextureHero);
+    }
+    Hero.prototype.onInitialize = function () {
+        this.addDrawing(Resources.TextureHero);
+    };
+    return Hero;
+})(ex.Actor);
+var Treasure = (function (_super) {
+    __extends(Treasure, _super);
+    function Treasure(x, y, width, height, color) {
+        _super.call(this, x, y, width, height, color);
+    }
+    return Treasure;
+})(ex.Actor);
 /// <reference path="resources.ts" />
+/// <reference path="hero.ts" />
+/// <reference path="treasure.ts" />
 var game = new ex.Engine({
     canvasElementId: "game",
     width: 960,
@@ -22,4 +48,10 @@ game.start(loader).then(function () {
     map.addDrawing(Resources.TextureMap);
     map.anchor.setTo(0, 0);
     game.add(map);
+    var hero = new Hero(50, 50, 50, 50, ex.Color.Red);
+    game.add(hero);
+    var treasure = new Treasure(game.width - 50, game.height - 50, 50, 50, ex.Color.Yellow);
+    game.add(treasure);
+    treasure.setZIndex(-1);
+    hero.moveTo(treasure.x, treasure.y, 100);
 });
