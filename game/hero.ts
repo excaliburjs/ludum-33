38e0a58319dbdc70fb24a79937d95089e3be1a0b
Josh Edeen <josh.edeen@gmail.com>
2015-08-21 23:30:29 -0500

    fixed sprite initialization

--- game/hero.ts
+++ game/hero.ts
@@ -2,11 +2,11 @@ class Hero extends ex.Actor {
    
    constructor(x: number, y: number, width: number, height: number, color?: ex.Color) {
       super(x, y, width, height, color);
-      // this.addDrawing(Resources.TextureHero);
-   }
-   
-   onInitialize() {
       this.addDrawing(Resources.TextureHero);
    }
+   
+   // onInitialize() {
+   //    this.addDrawing(Resources.TextureHero);
+   // }
       
 }
