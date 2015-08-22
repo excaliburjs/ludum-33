5e9f0077cbe686ed8b6829ac1e39b07831f32856
Josh Edeen <josh.edeen@gmail.com>
2015-08-22 12:50:19 -0500

    monster keeps track of heroes that it can hit

--- game/monster.ts
+++ game/monster.ts
@@ -6,6 +6,7 @@ class Monster extends ex.Actor {
    private _mouseY: number;
    
    private _rays: ex.Ray[];
+   private _attackable: Hero[]; // heroes that can be attacked during current update loop
    
    constructor(x, y){
       super(x, y, Config.MonsterWidth * 3, Config.MonsterHeight * 3);
@@ -13,6 +14,7 @@ class Monster extends ex.Actor {
       this._mouseX = 0;
       this._mouseY = 0;
       this._rays = new Array<ex.Ray>();
+      this._attackable = new Array<Hero>();
    }
    
    onInitialize(engine: ex.Engine): void {
@@ -34,16 +36,19 @@ class Monster extends ex.Actor {
       this.addDrawing(sprite);
       
       var yValues = new Array<number>(-0.62, -0.25, 0, 0.25, 0.62);
-      for (var i = 0; i < yValues.length; i++) {
-         var rayVector = new ex.Vector(1, yValues[i]);
+      _.forIn(yValues, (yValue) => {
+         var rayVector = new ex.Vector(1, yValue);
          var rayPoint = new ex.Point(this.x, this.y);
          var ray = new ex.Ray(rayPoint, rayVector);
          that._rays.push(ray);
-      }
+      });
    }
    
    public update(engine: ex.Engine, delta: number): void {
       super.update(engine, delta);
+      
+      this._attackable.length = 0;
+      this._detectAttackable();
       
       // clear move
       this.dx = 0;
@@ -72,18 +77,41 @@ class Monster extends ex.Actor {
 
       var prevRotation = this.rotation;
       this.rotation = new ex.Vector(this._mouseX - this.x, this._mouseY - this.y).toAngle();
-      //updating attack rays
-      _.forIn(this._rays, (ray) =>{
+      // updating attack rays
+      _.forIn(this._rays, (ray: ex.Ray) =>{
          ray.pos = new ex.Point(this.x, this.y);
          var rotationAmt = this.rotation - prevRotation;
          ray.dir = ray.dir.rotate(rotationAmt, new ex.Point(0, 0));
       });
    }
+   
+   private _detectAttackable() {
+      _.forIn(HeroSpawner.getHeroes(), (hero: Hero) => {
+         if (this._isHeroAttackable(hero)) {
+            this._attackable.push(hero);
+         }
+      });
+   }
+   
+   private _isHeroAttackable(hero: Hero) {
+      var heroLines = hero.getLines();
+      for (var i = 0; i < this._rays.length; i++) {
+         for (var j = 0; j < heroLines.length; j++) {
+            if (this._rays[i].intersect(heroLines[j]) > 0) {
+               return true;
+            }
+         }
+      }
+   }
+   
+   private _attack() {
+      
+   }
   
    public debugDraw(ctx: CanvasRenderingContext2D): void {
       super.debugDraw(ctx);
-      //Debugging draw for LOS rays on the enemy
-      _.forIn(this._rays, (ray) => {
+      // Debugging draw for attack rays
+      _.forIn(this._rays, (ray: ex.Ray) => {
          ctx.beginPath();
          ctx.moveTo(ray.pos.x, ray.pos.y);
          var end = ray.getPoint(Config.MonsterAttackRange);
