0dfbada5c2405ea80509b3e5f4cc899500740941
eonarheim <erik.onarheim@gmail.com>
2015-08-21 22:45:00 -0500

    Add monster and configs

--- game/config.ts
+++ game/config.ts
@@ -4,4 +4,9 @@ var Config = {
    MonsterWidth: 48,
    MonsterHeight: 48,
    MonsterSpeed: 300,
+   
+   CameraElasticity: .01,
+	CameraFriction: .21,
+	CameraShake: 7,
+	CameraShakeDuration: 800
 }
--- game/game.ts
+++ game/game.ts
@@ -12,8 +12,30 @@ _.forIn(Resources, (resource) => {
    loader.addResource(resource);
 });
 
+
+var monster: Monster = null;
+
+// mess with camera to lerp to the monster
+var cameraVel = new ex.Vector(0, 0);
+game.on('update', function(){
+	if(monster) {
+      var focus = game.currentScene.camera.getFocus().toVector();
+      var position = new ex.Vector(monster.x, monster.y);
+      
+      var stretch = position.minus(focus).scale(Config.CameraElasticity);
+      cameraVel = cameraVel.plus(stretch);
+      
+      var friction = cameraVel.scale(-1).scale(Config.CameraFriction);
+      cameraVel = cameraVel.plus(friction);
+      
+      focus = focus.plus(cameraVel);
+      game.currentScene.camera.setFocus(focus.x, focus.y);      
+   }
+});
+
+
 game.start(loader).then(() => {
-   
-   // magic here bro
-   
+   // magic here
+   var monster = new Monster(game.width/2, game.height/2);
+   game.add(monster);   
 });
