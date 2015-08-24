a5804b5bda6297809af30cfbaa112e6e98f522ed
Josh Edeen <josh.edeen@gmail.com>
2015-08-23 20:47:48 -0500

    updated hero slain sound effect

--- game/resources.ts
+++ game/resources.ts
@@ -2,7 +2,7 @@ var Resources = {
    
    AxeSwing: new ex.Sound('sounds/axe-swing.wav'),
    AxeSwingHit: new ex.Sound('sounds/axe-swing-hit-2.wav'),
-   BloodSpatter: new ex.Sound('sounds/blood-splatter-1.wav'),
+   BloodSpatter: new ex.Sound('sounds/hero-slain.wav'),
    HeroSwing: new ex.Sound('sounds/hero-swing.wav'),
    AnnouncerDefend: new ex.Sound('sounds/defend.wav'),
    
